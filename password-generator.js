0ff7b1555cca057bfbb060df0ebc3272bb70d077
David Midlo <dmidlo@gmail.com>
2020-03-06 11:29:25 -0600

    working onclick for button-toggle

--- password-generator.js
+++ password-generator.js
@@ -1,35 +1,54 @@
-// Unicode
-
-function addEvent(element, evnt, funct){
-   return element.addEventListener(evnt, funct, false);
+/////////////////////
+/////////// Functions
+/////////////////////
+function addEvent(element, evnt, funct) {
+  return element.addEventListener(evnt, funct);
 }
 
 function getElementObject(DOMid) {
-    return document.getElementById(DOMid)
+  return document.getElementById(DOMid);
 }
 
 function getTextInputValue(DOMid) {
-  return document.getElementById(DOMid).value;;
+  return document.getElementById(DOMid).value;
 }
 
 function setTextInputValue(DOMid, val) {
   document.getElementById(DOMid).value = val;
 }
 
+function bubbleStop(obj, type) {
+  if (type === "stopPropagation") {
+    obj.stopPropagation();
+  } else if (type === "preventDefault") {
+    obj.preventDefault();
+  }
+}
+
+function consoleLog(obj) {
+  console.log(obj);
+}
+
 function GetUnicode(params) {
   return;
 }
-
+////////////////////////////////////////////////////////
+/////////// Main Handler ///////////////////////////////
+////////////////////////////////////////////////////////
 function main() {
-  //Test Main Password Field  
-  setTextInputValue("passwordTextInput","Johnny Bravo");  
+  //Test Main Password Field
+  setTextInputValue("passwordTextInput", "Johnny Bravo");
   console.log(getTextInputValue("passwordTextInput"));
 
-  addEvent(getElementObject(""))
+  //Test Dropdown-Toggler click event
+  addEvent(getElementObject("dropdownToggleBtn"), "click", function (e){
+    bubbleStop(e, "preventDefault");
+    consoleLog(e);
+  });
 
-
-  return ;
+  return;
 }
-addEvent(document, "DOMContentLoaded", main())
-
-
+////////////////////////////////////////////////////////
+////////// Run /////////////////////////////////////////
+////////////////////////////////////////////////////////
+addEvent(document, "DOMContentLoaded", main());
